275fd1e724e68323d0af61a4c2071a0fb420822a
Candice Ariel <candicewangsa@gmail.com>
2023-05-12 17:48:52 +0700

    Styling

--- client/src/components/Kanban/Card.vue
+++ client/src/components/Kanban/Card.vue
@@ -1,9 +1,10 @@
 <template>
   <div class='c-kanban__card'>
     <p class='c-kanban__column-status c-kanban__card-title'>{{title}}</p>
-    <div>
+    <div class='c-kanban__card-status' :class="{ 'c-kanban__card-status_todo' : taskStatus === 1, 'c-kanban__card-status_in-progress' : taskStatus === 2}">
       <span v-if="taskStatus === 1">To Do</span>
       <span v-if="taskStatus === 2">In Progress</span>
+      <img src='../../assets/images/arrow-down-icon.svg' class='c-kanban__card-status-icon' />
     </div>
   </div>
 </template>
